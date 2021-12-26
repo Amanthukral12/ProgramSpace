c18fff95cba1f6d805590e24d87865f0a3144d9d
Aman Thukral <thukral618@gmail.com>
2021-12-26 17:00:14 +0530

    Adding logic for producthunt page

--- src/components/ProductHunt.jsx
+++ src/components/ProductHunt.jsx
@@ -1,7 +1,83 @@
+import {useState, useEffect} from 'react'
+import { ReactComponent as ThreeDots } from '../assets/svgs/threedots.svg'
 const ProductHunt = () => {
+    const [productList, setProductList] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+    const query = `{
+        posts() {
+            edges {
+                cursor,
+                node {
+                    id,
+                    description,
+                    tagline,
+                    url,
+                    votesCount,
+                    name,
+                    thumbnail {
+                        type,
+                        url
+                    }
+                }
+            }
+        }
+    }`
+    const opts = {
+        headers : {
+            Authorization : `Bearer ${import.meta.env.VITE_KEY}`,
+            Accept: 'application/json',
+            'Content-type': 'application/json',
+    
+            
+        },
+        method: 'POST',
+        mode: "cors",
+        body: JSON.stringify({ query })
+    }
+    const getProductList = async () => {
+        const response = await fetch('https://api.producthunt.com/v2/api/graphql', opts)
+        const trendingList = await response.json();
+        setIsLoading(false);
+        setProductList(trendingList.data.posts.edges);
+    }
+
+    useEffect(() => {
+        getProductList();
+    }, [])
     return (
         <div>
-            ProductHunt
+            {
+                (isLoading === true) ?
+                    <div>
+                        <ThreeDots />
+                    </div>
+                    :
+                    <div>
+                        {productList.map(product => {
+                            return(
+                                <div>
+                                    <a href={product.node.url}>
+                                        <div>
+                                            <img src={product.node.thumbnail.url} alt="" />
+                                            <div>
+                                                <h2>
+                                                    {product.node.name}
+                                                </h2>
+                                                <span>
+                                                    {product.node.tagline}
+                                                </span>
+                                            </div>
+                                            <div>
+                                                <span>{product.node.votesCount}</span>
+                                            </div>
+                                        </div>
+                                    </a>
+                                </div>
+                            )
+                        })}
+                    </div>
+                
+            }
         </div>
     )
 }
