2f6b863669a0d79c4ee41ada109f1512dff8643e
Aman Thukral <thukral618@gmail.com>
2021-12-28 02:10:07 +0530

    Adding styles for dev.to page

--- src/components/Dev.jsx
+++ src/components/Dev.jsx
@@ -2,6 +2,7 @@ import {useState, useEffect} from 'react'
 import { ReactComponent as ThreeDots } from '../assets/svgs/threedots.svg'
 import Header from './Header';
 import DevIcon from '../assets/images/dev.webp'
+import Fade from 'react-reveal/Fade';
 const Dev = () => {
     const [isLoading, setIsLoading] = useState(true);
     const [feedPost, setFeedPost] = useState([]);
@@ -32,16 +33,18 @@ const Dev = () => {
                         <ThreeDots />
                     </div>
                     :
-                    <div>
+                    <div className='flex flex-row flex-wrap justify-center p-2'>
                         {feedPost.map(post => {
                             return(
-                                <a href={post.url} key={post.id}>
-                                    <div>
-                                        <img src={post.cover_image} alt="" />
-                                        <p>{post.title}</p>
-                                        <p>{post.user.name}</p>
+                                <Fade bottom key={post.id}>
+                                <a href={post.url} target="_blank" rel="noreferrer">
+                                    <div className='w-72 h-auto py-auto shadow rounded-md bg-white p-4 text-left m-2'>
+                                        <img src={post.social_image === '' ? 'https://picsum.photos/seed/picsum/200/150' : post.social_image} alt="cover-image" className='rounded h-36' />
+                                        <p className='text-md font-medium mt-2'>{post.title}</p>
+                                        <p className='text-sm font-semibold mt-2'>{post.user.name}</p>
                                     </div>
                                 </a>
+                                </Fade>
                             )
                         })}
                     </div>
