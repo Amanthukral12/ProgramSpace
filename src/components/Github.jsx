76574d9ccad13738b0ca89eed5ff322ccedca02d
Aman Thukral <thukral618@gmail.com>
2021-12-29 02:47:35 +0530

    Adding styling for github page

--- src/components/Github.jsx
+++ src/components/Github.jsx
@@ -1,7 +1,10 @@
 import React, { useState, useEffect } from 'react'
 import { ReactComponent as ThreeDots } from '../assets/svgs/threedots.svg'
+import starIcon from '../assets/svgs/star.svg'
+import forkIcon from '../assets/svgs/fork.svg'
 import githubIcon from '../assets/images/github.png'
 import Header from './Header';
+import Fade from 'react-reveal/Fade';
 const Github = () => {
     const [repoList, setRepoList] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
@@ -28,16 +31,35 @@ const Github = () => {
                    <ThreeDots fill="#8B5CF6" />
                 </div>
                 :
-                <div>
+                <div className='flex flex-col justify-center px-8 py-2'>
                 {repoList.map(repo => {
                     return (
-                        <div>
-                            <span>{repo.name}</span>
-                            <div>{repo.description}</div>
-                            <div>{repo.language}</div>
-                            <div>{repo.stars}</div>
-                            <div>{repo.forks}</div>
-                        </div>
+                        <Fade bottom key={repo.name}>
+                            <div className='bg-white p-4 px-8 m-2 text-left rounded-sm'>
+                            <a href={repo.url} target="_blank" rel="noreferrer">
+                            <h3 className='text-xl font-semibold text-blue-500'>{repo.name}</h3>
+                            <span className='text-md'>{repo.description}</span>
+                            <div className='flex flex-row my-2'>
+                                <div className='flex mr-2'>
+                                    <div className='h-4 w-4 rounded-full m-1 mr-2' style={{backgroundColor: `${repo.languageColor}`}}></div>
+                                    <h3>{repo.language}</h3>
+                                </div>
+
+                                <div className='flex mr-2'>
+                                    <img src={starIcon} alt="starIcon" 
+                                    className='h-4 w-4 m-1' />
+                                    <h3>{repo.stars}</h3>
+                                </div>
+
+                                <div className='flex mr-2'>
+                                    <img src={forkIcon} alt="forkIcon"
+                                    className='h-4 w-4 m-1' />
+                                    <h3>{repo.forks}</h3>
+                                </div>
+                            </div>
+                            </a>
+                            </div>
+                        </Fade>
                     )
                 })}
                 </div>
