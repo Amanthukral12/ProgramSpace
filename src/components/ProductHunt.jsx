3fed522386fb9b8b7e3d1f58ba8fc271f6f5b9c1
Aman Thukral <thukral618@gmail.com>
2021-12-29 18:57:25 +0530

    Adding styling for product hunt page

--- src/components/ProductHunt.jsx
+++ src/components/ProductHunt.jsx
@@ -2,6 +2,8 @@ import {useState, useEffect} from 'react'
 import { ReactComponent as ThreeDots } from '../assets/svgs/threedots.svg'
 import productIcon from '../assets/images/producthunt.png'
 import Header from './Header'
+import Fade from 'react-reveal/Fade';
+import upvoteIcon from '../assets/svgs/upvote.svg'
 const ProductHunt = () => {
     const [productList, setProductList] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
@@ -55,31 +57,37 @@ const ProductHunt = () => {
                 />
             {
                 (isLoading === true) ?
-                    <div>
-                        <ThreeDots />
+                    <div className='flex justify-center my-48 text-7xl'>
+                        <ThreeDots fill="#8B5CF6" />
                     </div>
                     :
                     <div>
                         {productList.map(product => {
                             return(
-                                <div>
-                                    <a href={product.node.url}>
-                                        <div>
-                                            <img src={product.node.thumbnail.url} alt="" />
-                                            <div>
-                                                <h2>
+                                <Fade bottom key={product.node.id}>
+                                <div className='m-6 bg-white p-4 rounded text-left'>
+                                    <a href={product.node.url} target="_blank" rel="noreferrer">
+                                        <div className='flex flex-row '>
+                                            <img src={product.node.thumbnail.url} alt="product_image" 
+                                            className='md:h-20 md:w-20 h-14 w-14 border-2 mr-4 rounded-sm' />
+                                            
+                                            <div className='flex flex-col my-auto'>
+                                                <h2 className='md:text-xl text-sm font-semibold'>
                                                     {product.node.name}
                                                 </h2>
-                                                <span>
+                                                <p className='text-xs md:text-sm'>
                                                     {product.node.tagline}
-                                                </span>
+                                                </p>
                                             </div>
-                                            <div>
-                                                <span>{product.node.votesCount}</span>
+                                            <div className='ml-auto border-2 text-center my-2 px-4 h-14 w-14 flex flex-col justify-center'>
+                                                <img src={upvoteIcon} alt="upvoteCon" className='h-4 w-4 mx-auto' />
+                                                <h3>{product.node.votesCount}</h3>
                                             </div>
+                                            
                                         </div>
                                     </a>
                                 </div>
+                                </Fade>
                             )
                         })}
                     </div>
