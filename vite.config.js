a10ee729f2f878bc8672fd4c427092be1b58ff98
Aman Thukral <thukral618@gmail.com>
2022-01-04 00:20:23 +0530

    Adding vite compression plugin

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import svgrPlugin from 'vite-plugin-svgr'
-
+import viteCompression from 'vite-plugin-compression';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
+    viteCompression(),
     svgrPlugin({
       svgrOptions: {
         icon: true,
